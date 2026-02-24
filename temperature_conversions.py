79655f8f209d63f584183e6033530b0352cc6be2
kaakarakai <harshavardhangunnu@gmail.com>
2026-02-24 11:55:18 +0530

    Update temperature_conversions.py

--- temperature_conversions.py
+++ temperature_conversions.py
@@ -1,3 +1,7 @@
 x = float(input("enter your temparature in farenheit: "))
 c = (5/9)*(x-32)
 print(c)
+# celcius to fahrenheit
+x = float(input("enter your temparature in celcius: "))
+f = (9/5)*(32+x)
+print(f)
